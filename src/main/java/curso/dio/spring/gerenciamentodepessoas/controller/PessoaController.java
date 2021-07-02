cc53682df22eb93c6ec300abe373484869c7ad69
Guilherme C. Tonello <guitonello@outlook.com.br>
2021-07-02 14:55:21 -0300

    Update de pessoa

--- src/main/java/curso/dio/spring/gerenciamentodepessoas/controller/PessoaController.java
+++ src/main/java/curso/dio/spring/gerenciamentodepessoas/controller/PessoaController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -50,5 +51,14 @@ public class PessoaController {
 	public void deletarPessoa(@PathVariable("id") Long id) throws PessoaNotFoundException {
 		pessoaService.deleteById(id);
 	}
+	
+	@PutMapping("{id}")
+	@ResponseStatus(code = HttpStatus.OK)
+	public MessageResponseDTO atualizarPessoa(
+			@PathVariable("id") Long id, 
+			@RequestBody PessoaDTO pessoaDTO) throws PessoaNotFoundException {
+		return pessoaService.updateById(id, pessoaDTO);
+	}
+	
 
 }
